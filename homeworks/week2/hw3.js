4746ce04a024f62cdc33e11e1d448097f067f750
Fayang Chung <york870198@gmail.com>
2021-04-20 21:55:03 +0800

    hw1~6 finished

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+	result = "";
+	for(let i =0; i<str.length; i++){
+		result = str[i] + result;
+	}
+	console.log(result);
 }
 
 reverse('hello');
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,9 @@
 function printFactor(n) {
-  
+	for(let i = 1; i<=(n/2); i++){
+		if(n%i==0){
+			console.log(i);
+		}
+	}
 }
 
 printFactor(10);
