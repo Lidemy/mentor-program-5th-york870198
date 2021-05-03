2a615e7d4901ae2ba884c8d3b528bcdaec5aac2c
Fayang Chung <york870198@gmail.com>
2021-05-03 15:11:14 +0800

    hw1 change

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -5,7 +5,7 @@ const got = require('got');
     const response = await got('https://lidemy-book-store.herokuapp.com/books?_limit=10')
     const resBody = JSON.parse(response.body)
     for (let i = 0; i < resBody.length; i++) {
-      console.log(`${resBody[i].id} + ${resBody[i].name}`)
+      console.log(`${resBody[i].id} ${resBody[i].name}`)
     }
   } catch (error) {
     console.log(error.response.body)
