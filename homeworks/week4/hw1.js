2987a69ae4bd81d92407c72601be0111cb482b6c
Fayang Chung <york870198@gmail.com>
2021-05-03 15:10:08 +0800

    hw1 finished

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,13 @@
+const got = require('got');
+
+(async() => {
+  try {
+    const response = await got('https://lidemy-book-store.herokuapp.com/books?_limit=10')
+    const resBody = JSON.parse(response.body)
+    for (let i = 0; i < resBody.length; i++) {
+      console.log(`${resBody[i].id} + ${resBody[i].name}`)
+    }
+  } catch (error) {
+    console.log(error.response.body)
+  }
+})()
